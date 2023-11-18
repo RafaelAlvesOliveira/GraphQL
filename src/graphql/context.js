1a7ca6b535c4594cf8bd271449f585d557987814
RafaelALvesOliveira <faelalvesoliveira06@gmail.com>
2023-11-17 23:09:41 -0300

    Separando o dataloader em um arquivo separado, e separando as chamadas via API_URL em dois arquivos dentro das respectivas pastas, os arquivos se chamam util.js e estão dentro da pasta post e da pasta user. Criação do arquivo .env para criar a chamada do localhost a partir dele.

--- src/graphql/context.js
+++ src/graphql/context.js
@@ -1,10 +1,14 @@
 import fetch from 'node-fetch';
+import { getPosts } from './post/utils';
+import { getUsers } from './user/utils';
+import { makeUserDataLoader } from './user/dataloaders';
 
-const API_URL = 'http://localhost:3000';
+const API_URL = process.env.API_URL;
 
 export const context = () => {
   return {
-    getUsers: (path = '/') => fetch(API_URL + '/users' + path),
-    getPosts: (path = '/') => fetch(API_URL + '/posts' + path),
+    userDataLoader: makeUserDataLoader(getUsers(fetch)),
+    getUsers: getUsers(fetch),
+    getPosts: getPosts(fetch),
   };
 };
