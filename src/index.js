71800fadc1fcf007d8c77256d945b94d6432c2bf
RafaelALvesOliveira <faelalvesoliveira06@gmail.com>
2023-11-20 21:30:12 -0300

    Implementando RestDataSource em post, para evitar a chamada duplicada, caching entre outras coisas.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import { ApolloServer } from 'apollo-server';
 import { context } from './graphql/context';
 import { resolvers, typeDefs } from './graphql/schema';
+import { PostsApi } from './graphql/post/datasources';
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context,
+  dataSources: () => {
+    return {
+      postApi: new PostsApi(),
+    };
+  },
 });
 
 server.listen(4003).then(({ url }) => {
